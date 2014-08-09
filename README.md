bf0acc0abc29f1952e0dcd286a7ac325ddb243fb
Lane Aasen <laneaasen@gmail.com>
2014-08-09 16:08:24 -0700

    glfw window working

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 ### Installing GLFW
 
 https://github.com/shurcooL/reusable-commands/blob/ed33ae496f36aaea735a1d183f77e833c92a9f3d/go-gl_glfw3_install.sh#L19-L32
+
+### Installing GLEW
+
+`sudo apt-get install libglew-dev`
--- main.go
+++ main.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"fmt"
+	glfw "github.com/go-gl/glfw3"
+)
+
+func glfwErrorCallback(err glfw.ErrorCode, description string) {
+	fmt.Println("%v: %v", err, description)
+}
+
+const (
+	width  = 640
+	height = 400
+	title  = "Voxel Party"
 )
 
 func main() {
 	fmt.Println("Hello, world")
+
+	glfw.SetErrorCallback(glfwErrorCallback)
+
+	if !glfw.Init() {
+		panic("cannot initialize GLFW")
+	}
+
+	defer glfw.Terminate()
+
+	window, err := glfw.CreateWindow(width, height, title, nil, nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+
+	for !window.ShouldClose() {
+		//Do OpenGL stuff
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
 }
